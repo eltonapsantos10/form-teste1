ef8ea4905274dc7b6f1489e21403548a82648502
eltonapsantos10 <67808898+eltonapsantos10@users.noreply.github.com>
2021-03-24 15:49:19 -0300

    adicionando campo telefone

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function home() {
   return <div>
 
-    <form
+    <form id="form-geral"
       name="contact v1"
       method="post"
       data-netlify="true"
@@ -16,40 +16,46 @@ export default function home() {
       />
 
       <div hidden>
-        <input name="bot-field"/>
+        <input name="bot-field" />
 
       </div>
 
-    <div>
-      <label> First Name <br/>
-      <input type="text" name="first-name"/>
-      </label>
-    </div>
+      <div>
+        <label> Nome <br />
+          <input type="text" name="first-name" autoComplete="off"/>
+        </label>
+      </div>
 
 
-    <div>
-      <label> Last Name <br/>
-      <input type="text" name="last-name"/>
-      </label>
-    </div>
+      <div>
+        <label> Sobrenome <br />
+          <input type="text" name="last-name" autoComplete="off" />
+        </label>
+      </div>
 
-    <div>
-      <label htmlFor="email">Email</label> <br/>
-      <input id="email" type="email" name="email"/>
-    </div>
+      <div>
+        <label> Telefone  <br />
+          <input type="text" name="last-name" autoComplete="off" />
+        </label>
+      </div>
 
-
-
-    <div>
-    <label> Any comments? <br/>
-    <textarea name="comments"></textarea>
-
-    </label>
+      <div>
+        <label htmlFor="email">Email</label> <br />
+        <input id="email" type="email" name="email" autoComplete="off" />
+      </div>
 
 
-    </div>
 
-      <button type="submit">Submit The Results</button>
+      <div>
+        <label> Comentário <br />
+          <textarea name="comments"></textarea>
+
+        </label>
+
+
+      </div>
+
+      <button type="submit">Enviar Informações</button>
 
 
 
