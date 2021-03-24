cdeae502c45e114db460cf7559fa5cf5832bb683
eltonapsantos10 <67808898+eltonapsantos10@users.noreply.github.com>
2021-03-23 22:34:46 -0300

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function home() {
+  return <div>
+
+    <form
+      name="contact v1"
+      method="post"
+      data-netlify="true"
+      onSubmit="submit"
+    >
+
+      <input type="hidden" name="form-name" value="contact v1"
+
+      />
+
+    <div>
+      <label> First <br/>
+      <input type="text" name="first-name"/>
+      </label>
+    </div>
+
+
+    <div>
+      <label htmlFor="email">Email</label> <br/>
+      <input id="email" type="email" name="email"/>
+    </div>
+
+
+
+    <div>
+    <label> Any comments? <br/>
+    <textarea name="comments"></textarea>
+
+    </label>
+
+
+    </div>
+
+      <button type="submit">Submit The Results</button>
+
+
+
+
+    </form>
+
+  </div>
+
+
+
+}
+
+
