4b5b1e93a5e57fa59f77dfe9cde67eb6c17fa3b4
eltonapsantos10 <67808898+eltonapsantos10@users.noreply.github.com>
2021-03-25 12:44:38 -0300

    inclui link para voltar para home

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,7 +4,10 @@ export default function home() {
   return <div>
 
     <p id="text-resposta">Informações enviadas! Assim que possível retornaremos.</p>
+    <br></br>
+    <Link to="/home/">1</Link> <br />
 
   </div>
+
 
 }
